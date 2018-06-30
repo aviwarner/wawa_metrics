bbec1c94f461342d0031702d66a35392fd68d73f
Avi Warner <aviwarner@gmail.com>
2018-06-30 15:45:37 -0700

    events creation API setup

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   get 'about' => 'welcome#about'
   root to: "welcome#index"
 
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
